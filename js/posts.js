b0dad2ed8997884c48038c08926b340bceec461d
유건주 <gunjudavid@naver.com>
2024-10-29 17:02:56 +0900

    setting up

--- js/posts.js
+++ js/posts.js
@@ -1,4 +1,5 @@
 function toggleDropdown() {
-    var dropdown = document.getElementById("dropdown-menu");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    var dropdown = document.getElementById('dropdown-menu');
+    dropdown.style.display =
+        dropdown.style.display === 'block' ? 'none' : 'block';
 }
